e8086d993e45aaca0661ed0bd09726c06b4dc6c0
stormjoannes <storm.joannes@student.hu.nl>
2024-10-06 18:21:47 +0200

    update prints

--- assignment_1a/DecisionTreeClassifier.py
+++ assignment_1a/DecisionTreeClassifier.py
@@ -13,6 +13,7 @@ class DecisionTreeDialogClassifier:
         self.deduplicated_data = remove_duplicates(self.labeled_data)
         self.vectorizer = None
         self.clf_tree = None
+        self.print_output = False
 
     def train_and_evaluate(self, labeled_lines, description):
         X, labels, self.vectorizer = preprocess_data(labeled_lines, method="count")
@@ -23,23 +24,29 @@ class DecisionTreeDialogClassifier:
 
         y_pred = self.clf_tree.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
-        print(f"{description} Accuracy: {accuracy * 100:.2f}%")
-        print(classification_report(y_test, y_pred, zero_division=1))
+        if self.print_output:
+            print(f"{description} Accuracy: {accuracy * 100:.2f}%")
+            print(classification_report(y_test, y_pred, zero_division=1))
 
         return accuracy
 
     def run(self):
         # Calculate accuracy for original data
-        print("Evaluating on original data:")
         self.train_and_evaluate(self.labeled_data, "Original Data")
 
         # Calculate accuracy for deduplicated data
-        print("\nEvaluating on deduplicated data:")
         self.train_and_evaluate(self.deduplicated_data, "Deduplicated Data")
 
 
 # Usage Example
 decision_tree_classifier = DecisionTreeDialogClassifier('data/dialog_acts.dat')
+
+# print outputs if running as a script
+if __name__ == "__main__":
+    decision_tree_classifier.print_output = True
+else:
+    decision_tree_classifier.print_output = False
+
 decision_tree_classifier.run()
 
 # Expose vectorizer and clf_tree for dialog system
